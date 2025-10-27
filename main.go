b2d736f8bedc88df276bb034e2ed6d47870a1031
Artem Veber <veberav1201@yandex.ru>
2025-10-27 23:32:47 +0700

    Add song struct

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 	"strings"
 )
 
+type Song struct {
+	Title    string
+	Filename string
+	Seconds  int
+}
+
 func main() {
 	if len(os.Args) == 1 || !strings.HasSuffix(os.Args[1], ".pls") {
 		fmt.Printf("usage: %s <file.pls>\n", filepath.Base(os.Args[0]))
