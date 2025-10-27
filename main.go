e855e134ea896ee4af5e757318a9f1bfa01f0a47
Artem Veber <veberav1201@yandex.ru>
2025-10-27 23:31:13 +0700

    Add read file func

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) == 1 || !strings.HasPrefix(os.Args[1], ".pls") {
+	if len(os.Args) == 1 || !strings.HasSuffix(os.Args[1], ".pls") {
 		fmt.Printf("usage: %s <file.pls>\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
+	}
+
+	if rawbytes, err := os.ReadFile(os.Args[1]); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(string(rawbytes))
 	}
 }
